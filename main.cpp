88e6b4e4c7bda0c0994225ecb1748ba6634073dd
takei_yuushin0831 <k021g1262@g.neec.ac.jp>
2023-02-01 16:16:13 +0900

    3_3kadai

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
 	while (true)
 	{
 		//‘±‚¯‚é‚©‚Ç‚¤‚©“ü—Í‚³‚¹‚é
-		printf("0:exit 1:continue ==>");
+		printf("0:exit 1:continue  ==>");
 		scanf_s("%d", &input);
 		if (input == 0)break;
 
